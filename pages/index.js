a43892dfea949279cc13be462a2caf3aa6ea89c7
DNC\carlo <carlosmrtzow8@gmail.com>
2022-03-05 15:33:29 -0400

    finish home page in desktop

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Video from "../components/Video";
 import Menu from "../components/Menu";
 import Credits from "../components/Credits";
+
 export default function Home() {
   return (
     <>
